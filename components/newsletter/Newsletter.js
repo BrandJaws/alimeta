b6df2083107258a5edbccb703f1911e497f74773
AliRaza52 <frdev52.brandjaws@gmail.com>
2022-12-02 19:37:18 +0500

    fixed signature issues on mobile

--- components/newsletter/Newsletter.js
+++ components/newsletter/Newsletter.js
@@ -17,7 +17,7 @@ function Newsletter() {
                             <input type="text" className="form-control" placeholder='Enter your email address'></input>
                         </form>
                         <p>By signing up you agree to receive communications via email.<br />For more information please refer to our Privacy Policy.</p>
-                        <Image src={Signature} alt="Logo" />
+                        <Image className='desktop-only' src={Signature} alt="Logo" />
                     </div>
                 </div>
             </section>
