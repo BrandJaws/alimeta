2bcabb309ce612444e1c2c0abfe2ede7b2a74a03
AliRaza52 <frdev52.brandjaws@gmail.com>
2022-12-07 20:33:22 +0500

    form added

--- components/newsletter/Newsletter.js
+++ components/newsletter/Newsletter.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from "next/image";
 import Heading from "../../public/images/subscribe-heading.png";
 import Signature from "../../public/images/signature.png";
+import { useHubspotForm } from 'next-hubspot';
+
+const HubspotForm = (props) => {
+    useHubspotForm({
+        region: props.region,
+        portalId: props.portalId,
+        formId: props.formId,
+        target: '#hubspot-form-wrapper'
+    });
+
+    return (
+        <div id="hubspot-form-wrapper" />
+    )
+}
 
 function Newsletter() {
     return (
@@ -12,11 +26,12 @@ function Newsletter() {
                         <Image src={Heading} alt="Logo" />
                         <h4>Share your details and be the first to know how to go big, with AniMeta.</h4>
                     </div>
-                    <div className='max-width-1000'>
-                        <form>
-                            <div><input type="text" className="form-control" placeholder='Enter your email address'></input></div>
-                            <div><input type="submit" className='btn-submit' value="SUBMIT"></input></div>
-                        </form>
+                    <div className='max-width-1000'> 
+                        <HubspotForm 
+                            region="na1"
+                            portalId="23140983"
+                            formId='87dfc11d-6610-4a0f-a5a0-09db919fec0f'
+                        />  
                         <p>By signing up you agree to receive communications via email.</p>
                         <div className='desktop-only'>
                             <Image src={Signature} alt="Logo" />
