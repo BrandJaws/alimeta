52d7d4e1a22da930871a73af6042afca58d1eb11
AliRaza52 <frdev52.brandjaws@gmail.com>
2022-12-16 18:58:25 +0500

    fixed video link banner

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -1,23 +1,24 @@
-import Poster from "../../public/images/poster.jpg";
-if (typeof window !== 'undefined') {
-    var elem = document.getElementById("myvideo");
-}
+// import Poster from "../../public/images/poster.jpg";
+// if (typeof window !== 'undefined') {
+//     var elem = document.getElementById("myvideo");
+// }
 
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-}
+// function openFullscreen() {
+//     if (elem.requestFullscreen) {
+//         elem.requestFullscreen();
+//     } else if (elem.webkitRequestFullscreen) { /* Safari */
+//         elem.webkitRequestFullscreen();
+//     } else if (elem.msRequestFullscreen) { /* IE11 */
+//         elem.msRequestFullscreen();
+//     }
+// }
 
 function Banner() {
     return (
-        <div className='BannerSection' onClick={openFullscreen}>
+        <div className='BannerSection'>
+            {/* <div className="wistia_responsive_padding"><div className="wistia_responsive_wrapper"><iframe src="https://fast.wistia.net/embed/iframe/ani3jykos7?seo=false&videoFoam=true" title="Animeta R6 Video" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" className="wistia_embed" name="wistia_embed" msallowfullscreen width="100%" height="100%"></iframe></div></div> */}
             <video className='Bannervideo desktop-only' id="myvideo"  poster="https://animeta.ai/images/poster.jpg" autoPlay loop muted playsInline>
-                <source src="https://orla.ae/assets/video/banner_video_updated.mp4" type="video/mp4" />
+                <source src="https://d3e066o7tnnbih.cloudfront.net/banner_video_updated.mp4" type="video/mp4" />
             </video>
             <video className='Bannervideo mobile-only' id="myvideo"  poster="https://animeta.ai/images/poster.jpg" autoPlay loop muted playsInline>
                 <source src="https://orla.ae/assets/video/mobile-banner-video.mp4" type="video/mp4" />
--- pages/index.js
+++ pages/index.js
@@ -60,6 +60,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <script src='https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js' async></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js" async></script>
+        {/* <script src="https://fast.wistia.net/assets/external/E-v1.js" async></script> */}
       </Head>
       {/* <Welcome /> */}
       <section className='HeroScreen'>
