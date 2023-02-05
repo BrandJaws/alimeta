9e40de0a776fdec6a3c5a76263bba67862742924
AliRaza52 <frdev52.brandjaws@gmail.com>
2023-02-05 22:38:46 +0500

    animated logo added

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -41,7 +41,11 @@ function Footer() {
                     <div className='footer-right'>
                         <div className='LogoFooter'>
                             {/* <div className='gifLogo'></div> */}
-                            <Image src={FooterLogo} className="lazyload" alt="FooterLogo" />
+                            {/* <Image src={FooterLogo} className="lazyload" alt="FooterLogo" /> */}
+                            <video className='FooterVideo' autoPlay loop muted playsInline>
+                                {/* <source src="https://d3e066o7tnnbih.cloudfront.net/mobile-banner-video.mp4" type="video/mp4" /> */}
+                                <source src="https://orla.ae/assets/video/animated-footer-logo.mp4" type="video/mp4" />
+                            </video>
                         </div>
                         {/* <div className='footerLinks'>
                             <ul>
