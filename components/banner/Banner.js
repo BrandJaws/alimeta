56c30afc382f12bdc2bb772c7b49af1efec38e89
AliRaza52 <frdev52.brandjaws@gmail.com>
2023-02-07 17:35:04 +0500

    fixed video

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -16,11 +16,12 @@
 function Banner() {
     return (
         <div className='BannerSection'>
-            <video className='Bannervideo desktop-only' id="myvideo"  poster="https://animeta.ai/images/poster.jpg" autoPlay loop muted playsInline>
+            <video className='Bannervideo desktop-only' id="myvideo"  autoPlay loop muted playsInline>
                 <source src="https://d3e066o7tnnbih.cloudfront.net/banner_video_updated.mp4" type="video/mp4" />
                 {/* <source src="https://orla.ae/assets/video/banner_video_updated.mp4" type="video/mp4" /> */}
             </video>
-            <video className='Bannervideo mobile-only' poster="https://animeta.ai/images/cover-mobile.jpg" autoPlay loop muted playsInline>
+            {/* poster="https://animeta.ai/images/cover-mobile.jpg" */}
+            <video className='Bannervideo mobile-only'  poster="https://animeta.ai/images/poster.jpg"  autoPlay loop muted playsInline>
                 <source src="https://orla.ae/assets/video/banner_video_updated.mp4" />
             </video>
         </div>
