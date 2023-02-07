7c4d51836ad58ee7154fc92c64e815dbc39f4723
AliRaza52 <frdev52.brandjaws@gmail.com>
2023-02-07 18:10:08 +0500

    updated video

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -21,8 +21,8 @@ function Banner() {
                 {/* <source src="https://orla.ae/assets/video/banner_video_updated.mp4" type="video/mp4" /> */}
             </video>
             {/* poster="https://animeta.ai/images/cover-mobile.jpg" */}
-            <video className='Bannervideo mobile-only'  poster="https://animeta.ai/images/poster.jpg"  autoPlay loop muted playsInline>
-                <source src="https://d3e066o7tnnbih.cloudfront.net/banner_video_updated.mp4" />
+            <video className='Bannervideo mobile-only'  poster="https://animeta.ai/images/cover-mobile.jpg"  autoPlay loop muted playsInline>
+                <source src="https://orla.ae/assets/video/vertical.mp4" />
             </video>
         </div>
     )
