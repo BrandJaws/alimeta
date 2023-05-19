85f81bdb2681552b4743eefb7dc29f1f128a0cf0
mohsinikramvu <mohsin.brandjaws@gmail.com>
2023-05-19 23:53:21 +0500

    add blog page

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+export const blogCategories = [
+    {
+        name: "All Topic",
+        slug: "All Topic"
+    },
+    {
+        name: "Corporate PR",
+        slug: "Corporate PR"
+    },
+    {
+        name: "Creator PR",
+        slug: "Creator PR"
+    }
+]
